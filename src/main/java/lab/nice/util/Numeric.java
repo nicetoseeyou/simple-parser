5cc9b58285f97d7403eeccf3eee95ed3a307f35c
nicetoseeyou <hello4world@yeah.net>
2019-03-26 23:54:13 +0800

    fix low-order number (after dot) comparison

--- src/main/java/lab/nice/util/Numeric.java
+++ src/main/java/lab/nice/util/Numeric.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Numeric {
-    private static final String NUMERIC_REGEX = "^(\\-|\\+)?(\\d+)(\\.\\d+)?$";
+    private static final String NUMERIC_REGEX = "^([-+])?(\\d+)(\\.\\d+)?$";
     private static final String ZERO_LEADING_REGEX = "^0+(?!$)";
     private static final String ZERO_ENDING_REGEX = "0+?$";
     private static final String DOT_ENDING_REGEX = "[.]$";
@@ -27,28 +27,20 @@ public class Numeric {
      * <p>
      * Notice: input String must trim
      *
-     * @param number1
-     * @param number2
-     * @return
+     * @param number1 the left hand number
+     * @param number2 the right hand number
+     * @return code with meaning [0, EQUAL], [>0, GREATER_THAN], [<0, LESS_THAN]
      */
     public static int compare(String number1, String number2) {
         Pattern pattern = Pattern.compile(NUMERIC_REGEX);
         Matcher matcher = pattern.matcher(number1);
-        String[] group1 = extractMatcherGroup(matcher);
+        String[] group1 = splitNumber(matcher);
         matcher = pattern.matcher(number2);
-        String[] group2 = extractMatcherGroup(matcher);
-        System.out.print("g1=");
-        for (int i = 0; i < group1.length; i++) {
-            System.out.print(group1[i] + " ");
-        }
-        System.out.println();
-        System.out.print("g2=");
-        for (int i = 0; i < group2.length; i++) {
-            System.out.print(group2[i] + " ");
-        }
-        System.out.println();
-        System.out.println(afterDot(".00001", ".001"));
-        int beforeDot = beforeDot(group1[1], group2[1]);
+        String[] group2 = splitNumber(matcher);
+        int beforeDot = beforeDotCompare(group1[1], group2[1]);
+        System.out.println("BeforeDot: " + beforeDot);
+        int afterDot = afterDotCompare(group1[2], group2[2]);
+        System.out.println("AfterDot: " + afterDot);
         if (beforeDot == 0) {
 
         } else {
@@ -57,13 +49,34 @@ public class Numeric {
         return 0;
     }
 
-    private static int afterDot(String str1, String str2) {
-        String num1 = str1.replaceAll(ZERO_ENDING_REGEX, "").replaceAll(DOT_ENDING_REGEX, "");
-        String num2 = str2.replaceAll(ZERO_ENDING_REGEX, "").replaceAll(DOT_ENDING_REGEX, "");
+    /**
+     * Compare two unsigned integral number(before dot), the leading zero would be ignored.
+     *
+     * @param str1 the first unsigned integral number
+     * @param str2 the second unsigned integral number
+     * @return the code meaning: EQUAL(0), GREATER_THAN(>0), LESS_THAN(<0)
+     */
+    private static int afterDotCompare(String str1, String str2) {
+        String num1 = str1, num2 = str2;
+        if (num1 == null) {
+            num1 = "";
+        }
+        if (num2 == null) {
+            num2 = "";
+        }
+        num1 = num1.replaceAll(ZERO_ENDING_REGEX, "").replaceAll(DOT_ENDING_REGEX, "");
+        num2 = num2.replaceAll(ZERO_ENDING_REGEX, "").replaceAll(DOT_ENDING_REGEX, "");
         return num1.compareTo(num2);
     }
 
-    private static int beforeDot(String str1, String str2) {
+    /**
+     * Compare two dot leading decimal(after dot, dot included), the dot and ending zero would be ignored.
+     *
+     * @param str1 the first dot leading decimal
+     * @param str2 the second dot leading decimal
+     * @return the code meaning: EQUAL(0), GREATER_THAN(>0), LESS_THAN(<0)
+     */
+    private static int beforeDotCompare(String str1, String str2) {
         String noZero1 = str1.replaceFirst(ZERO_LEADING_REGEX, "");
         String noZero2 = str2.replaceFirst(ZERO_LEADING_REGEX, "");
         int length1 = noZero1.length(), lenght2 = noZero2.length();
@@ -80,7 +93,13 @@ public class Numeric {
         return 0;
     }
 
-    private static String[] extractMatcherGroup(Matcher matcher) {
+    /**
+     * Split number matched in matcher into three group: sign symbol, unsigned integral number(before dot), dot leading decimal(after dot, dot included)
+     *
+     * @param matcher matcher for numeric pattern
+     * @return String Array contains three group in order, [sign-symbol, unsigned-integral-number, dot-leading-decimal]
+     */
+    private static String[] splitNumber(Matcher matcher) {
         String[] group;
         if (matcher.find()) {
             int count = matcher.groupCount();
